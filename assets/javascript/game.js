9cb91bc929f0931235e16a019ecea875bcf1b8f8
Ariel <ariel.m.little@gmail.com>
2017-03-03 22:57:51 -0500

    timers mostly working, added start button, minor css changes, working on random array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
 
     var questions = [{
         question: "Baymax's motions are modeled after the movements of what animal?",
-        choices: ["baby penguins", "baby hippos", "kittens"],
+        choices: ["Baby Penguins", "Baby Hippos", "Kittens"],
         correct: 0,
         image: "assets/images/baymax-soccer.gif",
         audiosrc: "assets/audio/badalala.wav"
     }, {
         question: "The look and movement of microbots is based on",
-        choices: ["microbacteria", "swarms of ants", "magnets"],
+        choices: ["Microbacteria", "Swarms of Ants", "Magnets"],
         correct: 1,
         image: "assets/images/microbots.gif",
         audiosrc: "assets/audio/welcomenerdschool.wav"
@@ -21,7 +21,7 @@ $(document).ready(function() {
         choices: ["Marvel and Pixar", "Pixar and DC Comics", "Disney Animation and DC Comics", "Marvel and Disney Animation"],
         correct: 3,
         image: "assets/images/marvelbh6.jpeg",
-        audiosrc: "assets/audio/2botsenter.wav"
+        audiosrc: "assets/audio/badalala.wav"
     }, {
         question: "Who can be spotted on a Wanted sign at the police station?",
         choices: ["Lord Dingwall from Brave", "Flynn Rider from Tangled", "Prince Hans from Frozen"],
@@ -33,7 +33,7 @@ $(document).ready(function() {
         choices: ["The Jungle Book 2", "Mulan", "Treasure Planet"],
         correct: 0,
         image: "assets/images/bh6-kawaii.jpg",
-        audiosrc: "assets/audio/healthcarecompanion.wav"
+        audiosrc: "assets/audio/lollipop.wav"
     }, {
         question: "What Pixar character can be spotted as an action figure in Fred's library?",
         choices: ["Buzz Lightyear", "Wall-E", "Elastigirl from The Incredibles"],
@@ -42,7 +42,7 @@ $(document).ready(function() {
         audiosrc: "assets/audio/welcomenerdschool.wav"
     }, {
         question: "Who from the Marvel universe can be spotted as an action figure in Fred's library?",
-        choices: ["Sub-Mariner villain 'Orka'", "voodoo priest villain called 'Black Talon'", "Sleepwalker", "All of these guys and literally way more"],
+        choices: ["Sub-Mariner villain 'Orka'", "voodoo priest villain called 'Black Talon'", "Sleepwalker", "All of these guys and literally way more!"],
         correct: 3,
         image: "assets/images/fistbump.gif",
         audiosrc: "assets/audio/badalala.wav"
@@ -84,6 +84,17 @@ $(document).ready(function() {
         audiosrc: "assets/audio/healthcarecompanion.wav"
     }];
 
+    //array to hold random questions
+    var gameQuestions = [];
+
+    function pickQuestions() {
+        //from internet ???
+        questions.sort(function() { return 0.5 - Math.random() });
+        for(var i = 0; i < 10; i++){
+
+        }
+    };
+
 
     var currentQuestion = 0;
     var correctAnswers = 0;
@@ -91,36 +102,42 @@ $(document).ready(function() {
     var outOfTimeQuestions = 0;
     var number = 10;
     var intervalId;
-    var quizOver = false;
     var question;
     var questionClass;
     var choiceList;
     var numChoices;
     var audio = new Audio();
 
+    audio.src = "assets/audio/2botsenter.wav";
+    console.log(audio);
+    audio.load();
 
 
-
-// display first question with answer options and a timer counting down
-    displayQuestion();
+$("#start").click(function() {
+    $("#start").hide();
+    $(".question").html("<img src='assets/images/ready.gif'/>");
+    audio.play();
+    setTimeout (function(){
+        reset();
+    }, 9000);
+});
 
 
 
 // if player picks the correct answer, show a screen with a correct answer! message.
     function game() {
 
-        if (!quizOver) {
-
             $(".list-group-item").click(function() {
-                // stop();
                 var value = $(".list-group-item").index(this);
                 console.log(value);
                 if (value === questions[currentQuestion].correct) {
+                    stop();
                     console.log("clicked right answer!");
                     correctAnswers++;
                     breakTimeCorrect();
                 }
                 else {
+                    stop();
                     console.log("clicked wrong answer");
                     incorrectAnswers++;
                     breakTimeIncorrect();
@@ -129,7 +146,6 @@ $(document).ready(function() {
 
 
             });
-        };
     };  //end game function
 
     function playClip() {
@@ -150,12 +166,13 @@ $(document).ready(function() {
         $(".timeLeft").html("Correct!");
         $(".question").html("<img src='" + questions[currentQuestion].image + "'/>");
         $(".choiceList").hide();
-        $(".result").show().html("<p>Next Question</p>").click(function() {
-            $(".choiceList").show();
-            $(".timeLeft").html("timer here");
+        setTimeout(function() {
+            $(".timeLeft").html(number);
             $(".result").hide();
+            $(".choiceList").show();
             nextQuestion();
-        });
+        }, 8000);
+
     }
 
     function breakTimeIncorrect() {
@@ -164,12 +181,12 @@ $(document).ready(function() {
         $(".timeLeft").html("Sorry, the correct answer was " + questions[currentQuestion].choices[j]);
         $(".question").html("<img src='" + questions[currentQuestion].image + "'/>");
         $(".choiceList").hide();
-        $(".result").show().html("<p>Next Question</p>").click(function() {
-            $(".choiceList").show();
-            $(".timeLeft").html("timer here");
+        setTimeout(function() {
+            $(".timeLeft").html(number);
             $(".result").hide();
+            $(".choiceList").show();
             nextQuestion();
-        });
+        }, 8000);
     }
 
 
@@ -185,14 +202,14 @@ $(document).ready(function() {
         else {
             // stop();
             displayScore();
-            quizOver = true;
         };
     };
 
     function displayScore() {
+        $(".timeLeft").empty();
         $(".question").html("<img src='assets/images/bighero6-2.png'/>");
         $(".choiceList").hide();
-        $(".quizMessage").html("<button>Play Again</button>").show().click(function() {
+        $("#start").html("Play Again").show().click(function() {
             reset();
             });
         $(".result").html("Correct answers: " + correctAnswers + "<br> Incorrect answers: " + incorrectAnswers + "<br>Unanswered Questions: " + outOfTimeQuestions);
@@ -204,17 +221,17 @@ $(document).ready(function() {
         correctAnswers = 0;
         incorrectAnswers = 0;
         outOfTimeQuestions = 0;
-        quizOver = false;
-        $(".quizMessage").hide();
         $(".choiceList").show();
+        $(".timeLeft").show();
         $(".result").hide();
         displayQuestion();
     }
 
 
     function displayQuestion() {
+        $("#start").hide();
         //starts 30s timer
-        // timer();
+        timer();
         question = questions[currentQuestion].question;
         console.log("current question: " + question);
         //is .find necessary?
@@ -223,8 +240,8 @@ $(document).ready(function() {
         numChoices = questions[currentQuestion].choices.length;
         console.log("current answer index: " + questions[currentQuestion].correct);
 
-        // Set the questionClass text to the current question (.html instead?)
-        $(questionClass).text(question);
+        // Set the questionClass to the current question
+        $(questionClass).html(question);
 
         // Remove all current <li> elements (if any)
         $(choiceList).find(".list-group-item").remove();
@@ -240,54 +257,31 @@ $(document).ready(function() {
 
 
 
-//sets interval that runs decrement function 1x per second
-//     function timer() {
-//         intervalId = setInterval(decrement, 1000);
-//
-//     };
-//
-//     function decrement() {
-//         number--;
-//         $(".timeLeft").html("<h3>" + number + "</h3>");
-//         if (number === 0) {
-//             stop();
-//             number = 10;
-//             outOfTimeQuestions++;
-//             console.log("out of time");
-//             nextQuestion();
-//         };
-//     };
-//
-//     function stop() {
-//         clearInterval(intervalId);
-//
-//     };
+// sets interval that runs decrement function 1x per second
+    function timer() {
+        intervalId = setInterval(decrement, 1000);
+
+    };
+
+    function decrement() {
+        number--;
+        $(".timeLeft").html("<h3>" + number + "</h3>");
+        if (number === 0) {
+            stop();
+            number = 10;
+            outOfTimeQuestions++;
+            console.log("out of time");
+            breakTimeIncorrect();
+        };
+    };
+
+    function stop() {
+        clearInterval(intervalId);
+        number = 10;
+    };
 
 
 
 
 
 });  //end doc ready
-
-
-
-//trivia questions
-
-
-var questions = [{
-    question: "Baymax's motions are modeled after the movements of what animal?",
-    choices: ["baby penguins", "baby hippos", "baby humans"],
-    correct: 0
-}, {
-    question: "The look and movement of microbots is based on",
-    choices: ["microbacteria", "swarms of ants", "magnets"],
-    correct: 1
-}, {
-    question: "Big Hero 6 was the first collaboration between",
-    choices: ["Marvel and Pixar", "Pixar and DC Comics", "Disney Animation and DC Comics", "Marvel and Disney Animation"],
-    correct: 3
-}, {
-
-
-
-}];
